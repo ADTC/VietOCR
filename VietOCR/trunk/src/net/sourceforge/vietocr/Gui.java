550da9d106b7b09aed3d7fd33ea888887b5b9431
Quan Nguyen <nguyenq@users.noreply.github.com>
2008-10-26 04:07:51 +0000

    Added Apache 2 License statements to source files

--- VietOCR/trunk/src/net/sourceforge/vietocr/Gui.java
+++ VietOCR/trunk/src/net/sourceforge/vietocr/Gui.java
@@ -1,8 +1,18 @@
-/*
- * Gui.java
+/**
+ * Copyright @ 2008 Quan Nguyen
  *
- * Created on December 9, 2007, 12:21 PM
- */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 package net.sourceforge.vietocr;
 
 import java.io.*;
@@ -473,6 +483,7 @@ public class Gui extends javax.swing.JFrame {
         jToolBar2.add(jButtonOpen);
 
         jButtonScan.setText(bundle.getString("Scan")); // NOI18N
+        jButtonScan.setToolTipText(bundle.getString("Scan_Doc")); // NOI18N
         jButtonScan.setFocusable(false);
         jButtonScan.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         jButtonScan.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
--- VietOCR/trunk/src/net/sourceforge/vietocr/postprocessing/ViePP.java
+++ VietOCR/trunk/src/net/sourceforge/vietocr/postprocessing/ViePP.java
@@ -1,8 +1,19 @@
-/*
- * ViePP.java
+/**
+ * Copyright @ 2008 Quan Nguyen
  *
- * Created on December 24, 2007, 1:05 AM
- */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 package net.sourceforge.vietocr.postprocessing;
 
 import java.util.regex.*;
@@ -18,6 +29,7 @@ public class ViePP implements IPostProcessor {
     final String MARK = "[\u0306\u0302\u031B]?"; // (^+
     final String VOWEL = "[aeiouy]";
 
+    @Override
     public String postProcess(String text) {
         if (text.trim().length() == 0) {
             return text;
