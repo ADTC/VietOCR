893c2ab8015c9a09585c5dc08296c65361059fe6
Quan Nguyen <nguyenq@users.noreply.github.com>
2009-03-04 00:58:14 +0000

    Fixed minor problem with Windows EOL characters in Regex

--- VietOCR.NET/trunk/GUIWithFormat.cs
+++ VietOCR.NET/trunk/GUIWithFormat.cs
@@ -1,4 +1,19 @@
-﻿using System;
+/**
+ * Copyright @ 2008 Quan Nguyen
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -178,10 +193,9 @@ namespace VietOCR.NET
             int start = textBox1.SelectionStart;
 
             Regex regex = new Regex("(?<=\n|^)[\t ]+|[\t ]+(?=$|\n)");
-            string result = regex.Replace(textBox1.SelectedText, "");
+            string result = regex.Replace(textBox1.SelectedText.Replace(Environment.NewLine, "\n"), "");
             regex = new Regex("(?<=.)\n(?=.)");
-            result = regex.Replace(result, " ");
-
+            result = regex.Replace(result, " ").Replace("\n", Environment.NewLine);
             textBox1.SelectedText = result;
             textBox1.Select(start, result.Length);
         }
