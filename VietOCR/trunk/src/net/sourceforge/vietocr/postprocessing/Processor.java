613d8f4de7100f40c8221e8ba18e6207c612a118
Quan Nguyen <nguyenq@users.noreply.github.com>
2009-07-07 01:40:51 +0000

    Added support for text replacement based on entries read from a text file

--- VietOCR/trunk/src/net/sourceforge/vietocr/postprocessing/Processor.java
+++ VietOCR/trunk/src/net/sourceforge/vietocr/postprocessing/Processor.java
@@ -12,18 +12,37 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
-
+ */
 package net.sourceforge.vietocr.postprocessing;
+
+import java.io.File;
+import java.util.*;
+import org.apache.jetspeed.util.StringUtils;
 
 /**
  *
  * @author Quan Nguyen (nguyenq@users.sf.net)
  */
 public class Processor {
-    public static String postProcess(String text, String langCode)
-    {
+
+    public static String postProcess(String text, String langCode) {
         IPostProcessor processor = ProcessorFactory.createProcessor(ISO639.valueOf(langCode));
         return processor.postProcess(text);
+    }
+
+    public static String postProcess(String text, String langCode, File dangAmbigsPath) {
+        // postprocessor
+        StringBuffer strB = new StringBuffer(postProcess(text, langCode));
+
+        // replace text based on entries read from a DangAmbigs.txt file
+        Map<String, String> replaceRules = TextUtilities.loadMap(new File(dangAmbigsPath, langCode + ".DangAmbigs.txt"));
+        Set<String> set = replaceRules.keySet();
+        Iterator<String> iter = set.iterator();
+        while (iter.hasNext()) {
+            String key = iter.next();
+            String value = replaceRules.get(key);
+            strB = StringUtils.replaceAll(strB, key, value);
+        }
+        return strB.toString();
     }
 }
