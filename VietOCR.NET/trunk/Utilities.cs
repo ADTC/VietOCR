f861398a605a3198122af0f14b58819d27074e5b
Quan Nguyen <nguyenq@users.noreply.github.com>
2009-09-13 05:31:46 +0000

    Refined exception handling

--- VietOCR.NET/trunk/Utilities.cs
+++ VietOCR.NET/trunk/Utilities.cs
@@ -7,7 +7,7 @@ using ConvertPDF;
 namespace VietOCR.NET
 {
     class Utilities
-    {    
+    {
         /// <summary>
         /// Convert PDF to TIFF format.
         /// </summary>
@@ -15,41 +15,49 @@ namespace VietOCR.NET
         /// <returns>a multi-page TIFF image</returns>
         public static string ConvertPdf2Tiff(string inputPdfFile)
         {
-            try {
-                string[] pngFiles = ConvertPdf2Png(inputPdfFile);
+            string[] pngFiles = null;
+
+            try
+            {
+                pngFiles = ConvertPdf2Png(inputPdfFile);
                 string tiffFile = Path.GetTempFileName();
                 tiffFile = Path.ChangeExtension(tiffFile, ".tif");
-                
+
                 // put PNG images into a single multi-page TIFF image for return
                 ImageIOHelper.MergeTiff(pngFiles, tiffFile);
-
-                // delete temporary PNG images
-                foreach (string tempFile in pngFiles)
-                {
-                    new FileInfo(tempFile).Delete();
-                }
-
                 return tiffFile;
             }
-            catch (Exception e)
+            catch (ApplicationException ae)
             {
-                Console.WriteLine("ERROR: " + e.Message);
-                return null;
+                Console.WriteLine("ERROR: " + ae.Message);
+                throw;
+            }
+            finally
+            {
+                if (pngFiles != null)
+                {
+                    // delete temporary PNG images
+                    foreach (string tempFile in pngFiles)
+                    {
+                        File.Delete(tempFile);
+                    }
+                }
             }
         }
- 
+
         /// <summary>
         /// Convert PDF to PNG format.
         /// </summary>
         /// <param name="inputPdfFile"></param>
         /// <returns>an array of PNG images</returns>
-        public static string[] ConvertPdf2Png(string inputPdfFile) 
+        public static string[] ConvertPdf2Png(string inputPdfFile)
         {
             PDFConvert converter = new PDFConvert();
             converter.GraphicsAlphaBit = 4;
             converter.TextAlphaBit = 4;
             converter.ResolutionX = 300; // -r300
             converter.OutputFormat = "pnggray"; // -sDEVICE
+            converter.ThrowOnlyException = true; // rethrow exceptions
 
             string sOutputFile = string.Format("{0}\\workingimage%03d.png", Path.GetDirectoryName(inputPdfFile));
             bool success = converter.Convert(inputPdfFile, sOutputFile);
