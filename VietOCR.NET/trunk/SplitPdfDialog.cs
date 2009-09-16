0bab581eaa4df8548c739f775a4510d5b72fab2d
Quan Nguyen <nguyenq@users.noreply.github.com>
2009-09-16 23:23:34 +0000

    Added SplitPdf dialog

---
+++ VietOCR.NET/trunk/SplitPdfDialog.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VietOCR.NET
+{
+    public partial class SplitPdfDialog : Form
+    {
+        public SplitPdfDialog()
+        {
+            InitializeComponent();
+            disableBoxes(!this.radioButtonPages.Checked);
+        }
+
+        private void radioButtonPages_CheckedChanged(object sender, EventArgs e)
+        {
+            disableBoxes(false);
+        }
+
+        private void radioButtonRange_CheckedChanged(object sender, EventArgs e)
+        {
+            disableBoxes(true);
+        }
+
+        void disableBoxes(bool enabled)
+        {
+            this.textBoxNumOfPages.Enabled = enabled;
+            this.textBoxFrom.Enabled = !enabled;
+            this.textBoxTo.Enabled = !enabled;
+        }
+
+        private void buttonBrowseInput_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "PDF (*.pdf)|*.pdf";
+
+            if (dialog.ShowDialog() == DialogResult.Yes)
+            {
+                string filename = dialog.FileName;
+            }
+        }
+
+        private void buttonBrowseOutput_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "PDF (*.pdf)|*.pdf";
+
+            if (dialog.ShowDialog() == DialogResult.Yes)
+            {
+                string filename = dialog.FileName;
+            }
+        }
+    }
+}
