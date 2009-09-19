551607fbf62dea3d3f06e8949c6a249787aaf7e1
Quan Nguyen <nguyenq@users.noreply.github.com>
2009-09-19 14:19:10 +0000

    Additional localized text

--- VietOCR.NET/trunk/GUI.cs
+++ VietOCR.NET/trunk/GUI.cs
@@ -484,7 +484,7 @@ namespace VietOCR.NET
                 }
                 catch (ApplicationException e)
                 {
-                    MessageBox.Show(this, e.Message + "\nYou may need to split up the PDF file into smaller files before proceed.", strProgName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show(this, e.Message + Properties.Resources.suggest_split, strProgName, MessageBoxButtons.OK, MessageBoxIcon.Error);
                     return;
                 }
                 catch (Exception e)
