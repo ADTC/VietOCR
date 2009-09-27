291c338329bfc23bf7cf012c5e6873cd272a1fe0
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2009-09-27 15:27:39 +0000

    More localized text

--- VietOCR.NET/trunk/SplitPdfDialog.Designer.cs
+++ VietOCR.NET/trunk/SplitPdfDialog.Designer.cs
@@ -34,200 +34,136 @@
             this.textBoxFrom = new System.Windows.Forms.TextBox();
             this.textBoxTo = new System.Windows.Forms.TextBox();
             this.radioButtonPages = new System.Windows.Forms.RadioButton();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.radioButtonRange = new System.Windows.Forms.RadioButton();
+            this.labelFrom = new System.Windows.Forms.Label();
+            this.labelTo = new System.Windows.Forms.Label();
+            this.radioButtonFiles = new System.Windows.Forms.RadioButton();
             this.textBoxNumOfPages = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.labelNumOfPages = new System.Windows.Forms.Label();
             this.textBoxInput = new System.Windows.Forms.TextBox();
             this.textBoxOutput = new System.Windows.Forms.TextBox();
             this.buttonBrowseInput = new System.Windows.Forms.Button();
             this.buttonBrowseOutput = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.labelInput = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonSplit
             // 
-            this.buttonSplit.AccessibleDescription = null;
-            this.buttonSplit.AccessibleName = null;
-            resources.ApplyResources(this.buttonSplit, "buttonSplit");
-            this.buttonSplit.BackgroundImage = null;
             this.buttonSplit.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.buttonSplit.Font = null;
+            resources.ApplyResources(this.buttonSplit, "buttonSplit");
             this.buttonSplit.Name = "buttonSplit";
             this.buttonSplit.UseVisualStyleBackColor = true;
             this.buttonSplit.Click += new System.EventHandler(this.buttonSplit_Click);
             // 
             // buttonCancel
             // 
-            this.buttonCancel.AccessibleDescription = null;
-            this.buttonCancel.AccessibleName = null;
-            resources.ApplyResources(this.buttonCancel, "buttonCancel");
-            this.buttonCancel.BackgroundImage = null;
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Font = null;
+            resources.ApplyResources(this.buttonCancel, "buttonCancel");
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
             // 
             // textBoxFrom
             // 
-            this.textBoxFrom.AccessibleDescription = null;
-            this.textBoxFrom.AccessibleName = null;
             resources.ApplyResources(this.textBoxFrom, "textBoxFrom");
-            this.textBoxFrom.BackgroundImage = null;
-            this.textBoxFrom.Font = null;
             this.textBoxFrom.Name = "textBoxFrom";
             // 
             // textBoxTo
             // 
-            this.textBoxTo.AccessibleDescription = null;
-            this.textBoxTo.AccessibleName = null;
             resources.ApplyResources(this.textBoxTo, "textBoxTo");
-            this.textBoxTo.BackgroundImage = null;
-            this.textBoxTo.Font = null;
             this.textBoxTo.Name = "textBoxTo";
             // 
             // radioButtonPages
             // 
-            this.radioButtonPages.AccessibleDescription = null;
-            this.radioButtonPages.AccessibleName = null;
             resources.ApplyResources(this.radioButtonPages, "radioButtonPages");
-            this.radioButtonPages.BackgroundImage = null;
             this.radioButtonPages.Checked = true;
-            this.radioButtonPages.Font = null;
             this.radioButtonPages.Name = "radioButtonPages";
             this.radioButtonPages.TabStop = true;
             this.radioButtonPages.UseVisualStyleBackColor = true;
             this.radioButtonPages.CheckedChanged += new System.EventHandler(this.radioButtonPages_CheckedChanged);
             // 
-            // label1
+            // labelFrom
             // 
-            this.label1.AccessibleDescription = null;
-            this.label1.AccessibleName = null;
-            resources.ApplyResources(this.label1, "label1");
-            this.label1.Font = null;
-            this.label1.Name = "label1";
+            resources.ApplyResources(this.labelFrom, "labelFrom");
+            this.labelFrom.Name = "labelFrom";
             // 
-            // label2
+            // labelTo
             // 
-            this.label2.AccessibleDescription = null;
-            this.label2.AccessibleName = null;
-            resources.ApplyResources(this.label2, "label2");
-            this.label2.Font = null;
-            this.label2.Name = "label2";
+            resources.ApplyResources(this.labelTo, "labelTo");
+            this.labelTo.Name = "labelTo";
             // 
-            // radioButtonRange
+            // radioButtonFiles
             // 
-            this.radioButtonRange.AccessibleDescription = null;
-            this.radioButtonRange.AccessibleName = null;
-            resources.ApplyResources(this.radioButtonRange, "radioButtonRange");
-            this.radioButtonRange.BackgroundImage = null;
-            this.radioButtonRange.Font = null;
-            this.radioButtonRange.Name = "radioButtonRange";
-            this.radioButtonRange.UseVisualStyleBackColor = true;
-            this.radioButtonRange.CheckedChanged += new System.EventHandler(this.radioButtonRange_CheckedChanged);
+            resources.ApplyResources(this.radioButtonFiles, "radioButtonFiles");
+            this.radioButtonFiles.Name = "radioButtonFiles";
+            this.radioButtonFiles.UseVisualStyleBackColor = true;
+            this.radioButtonFiles.CheckedChanged += new System.EventHandler(this.radioButtonFiles_CheckedChanged);
             // 
             // textBoxNumOfPages
             // 
-            this.textBoxNumOfPages.AccessibleDescription = null;
-            this.textBoxNumOfPages.AccessibleName = null;
             resources.ApplyResources(this.textBoxNumOfPages, "textBoxNumOfPages");
-            this.textBoxNumOfPages.BackgroundImage = null;
-            this.textBoxNumOfPages.Font = null;
             this.textBoxNumOfPages.Name = "textBoxNumOfPages";
             // 
-            // label3
+            // labelNumOfPages
             // 
-            this.label3.AccessibleDescription = null;
-            this.label3.AccessibleName = null;
-            resources.ApplyResources(this.label3, "label3");
-            this.label3.Font = null;
-            this.label3.Name = "label3";
+            resources.ApplyResources(this.labelNumOfPages, "labelNumOfPages");
+            this.labelNumOfPages.Name = "labelNumOfPages";
             // 
             // textBoxInput
             // 
-            this.textBoxInput.AccessibleDescription = null;
-            this.textBoxInput.AccessibleName = null;
             resources.ApplyResources(this.textBoxInput, "textBoxInput");
-            this.textBoxInput.BackgroundImage = null;
-            this.textBoxInput.Font = null;
             this.textBoxInput.Name = "textBoxInput";
             // 
             // textBoxOutput
             // 
-            this.textBoxOutput.AccessibleDescription = null;
-            this.textBoxOutput.AccessibleName = null;
             resources.ApplyResources(this.textBoxOutput, "textBoxOutput");
-            this.textBoxOutput.BackgroundImage = null;
-            this.textBoxOutput.Font = null;
             this.textBoxOutput.Name = "textBoxOutput";
             // 
             // buttonBrowseInput
             // 
-            this.buttonBrowseInput.AccessibleDescription = null;
-            this.buttonBrowseInput.AccessibleName = null;
             resources.ApplyResources(this.buttonBrowseInput, "buttonBrowseInput");
-            this.buttonBrowseInput.BackgroundImage = null;
-            this.buttonBrowseInput.Font = null;
             this.buttonBrowseInput.Name = "buttonBrowseInput";
             this.buttonBrowseInput.UseVisualStyleBackColor = true;
             this.buttonBrowseInput.Click += new System.EventHandler(this.buttonBrowseInput_Click);
             // 
             // buttonBrowseOutput
             // 
-            this.buttonBrowseOutput.AccessibleDescription = null;
-            this.buttonBrowseOutput.AccessibleName = null;
             resources.ApplyResources(this.buttonBrowseOutput, "buttonBrowseOutput");
-            this.buttonBrowseOutput.BackgroundImage = null;
-            this.buttonBrowseOutput.Font = null;
             this.buttonBrowseOutput.Name = "buttonBrowseOutput";
             this.buttonBrowseOutput.UseVisualStyleBackColor = true;
             this.buttonBrowseOutput.Click += new System.EventHandler(this.buttonBrowseOutput_Click);
             // 
-            // label4
+            // labelInput
             // 
-            this.label4.AccessibleDescription = null;
-            this.label4.AccessibleName = null;
-            resources.ApplyResources(this.label4, "label4");
-            this.label4.Font = null;
-            this.label4.Name = "label4";
+            resources.ApplyResources(this.labelInput, "labelInput");
+            this.labelInput.Name = "labelInput";
             // 
-            // label5
+            // labelOutput
             // 
-            this.label5.AccessibleDescription = null;
-            this.label5.AccessibleName = null;
-            resources.ApplyResources(this.label5, "label5");
-            this.label5.Font = null;
-            this.label5.Name = "label5";
+            resources.ApplyResources(this.labelOutput, "labelOutput");
+            this.labelOutput.Name = "labelOutput";
             // 
             // SplitPdfDialog
             // 
             this.AcceptButton = this.buttonSplit;
-            this.AccessibleDescription = null;
-            this.AccessibleName = null;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = null;
             this.CancelButton = this.buttonCancel;
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.labelInput);
             this.Controls.Add(this.buttonBrowseOutput);
             this.Controls.Add(this.buttonBrowseInput);
             this.Controls.Add(this.textBoxOutput);
             this.Controls.Add(this.textBoxInput);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelNumOfPages);
             this.Controls.Add(this.textBoxNumOfPages);
-            this.Controls.Add(this.radioButtonRange);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButtonFiles);
+            this.Controls.Add(this.labelTo);
+            this.Controls.Add(this.labelFrom);
             this.Controls.Add(this.radioButtonPages);
             this.Controls.Add(this.textBoxTo);
             this.Controls.Add(this.textBoxFrom);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonSplit);
-            this.Font = null;
-            this.Icon = null;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SplitPdfDialog";
@@ -243,17 +179,17 @@
         private System.Windows.Forms.TextBox textBoxFrom;
         private System.Windows.Forms.TextBox textBoxTo;
         private System.Windows.Forms.RadioButton radioButtonPages;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.RadioButton radioButtonRange;
+        private System.Windows.Forms.Label labelFrom;
+        private System.Windows.Forms.Label labelTo;
+        private System.Windows.Forms.RadioButton radioButtonFiles;
         private System.Windows.Forms.TextBox textBoxNumOfPages;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelNumOfPages;
         private System.Windows.Forms.TextBox textBoxInput;
         private System.Windows.Forms.TextBox textBoxOutput;
         private System.Windows.Forms.Button buttonBrowseInput;
         private System.Windows.Forms.Button buttonBrowseOutput;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.Label labelOutput;
     }
 }
 
