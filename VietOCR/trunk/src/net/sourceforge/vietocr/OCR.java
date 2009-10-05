2651a7136970d15b840cfdc82a0da3e1a3de124a
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2009-10-05 00:16:32 +0000

    Added a method to handle a individual file. This enables returning of interim results for SwingWorker's publish method when processing multiple-page image

--- VietOCR/trunk/src/net/sourceforge/vietocr/OCR.java
+++ VietOCR/trunk/src/net/sourceforge/vietocr/OCR.java
@@ -12,8 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
-
+ */
 package net.sourceforge.vietocr;
 
 import java.io.*;
@@ -25,14 +24,13 @@ import java.util.*;
  * @author Quan Nguyen (nguyenq@users.sf.net)
  */
 public class OCR {
+
     private final String LANG_OPTION = "-l";
     private final String EOL = System.getProperty("line.separator");
-
     private String tessPath;
-
     final static String OUTPUT_FILE_NAME = "TessOutput";
     final static String FILE_EXTENSION = ".txt";
-    
+
     /** Creates a new instance of OCR */
     public OCR(String tessPath) {
         this.tessPath = tessPath;
@@ -47,7 +45,7 @@ public class OCR {
     String recognizeText(final List<File> tempImageFiles, final String lang) throws Exception {
         File tempTessOutputFile = File.createTempFile(OUTPUT_FILE_NAME, FILE_EXTENSION);
         String outputFileName = tempTessOutputFile.getPath().substring(0, tempTessOutputFile.getPath().length() - FILE_EXTENSION.length()); // chop the .txt extension
-        
+
         List<String> cmd = new ArrayList<String>();
         cmd.add(tessPath + "/tesseract");
         cmd.add(""); // placeholder for inputfile
@@ -58,24 +56,24 @@ public class OCR {
         ProcessBuilder pb = new ProcessBuilder();
         pb.directory(new File(System.getProperty("user.home")));
         pb.redirectErrorStream(true);
-            
+
         StringBuffer result = new StringBuffer();
 
         for (File tempImageFile : tempImageFiles) {
 //            ProcessBuilder pb = new ProcessBuilder(tessPath + "/tesseract", tempImageFile.getPath(), outputFileName, LANG_OPTION, lang);
-            
+
             cmd.set(1, tempImageFile.getPath());
             pb.command(cmd);
             Process process = pb.start();
-            
+
             int w = process.waitFor();
             System.out.println("Exit value = " + w);
-            
+
             if (w == 0) {
                 BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(tempTessOutputFile), "UTF-8"));
-                
+
                 String str;
-                
+
                 while ((str = in.readLine()) != null) {
                     result.append(str).append(EOL);
                 }
@@ -91,7 +89,7 @@ public class OCR {
                         break;
                     case 31:
                         msg = "Unsupported image format.";
-                        break;                        
+                        break;
                     default:
                         msg = "Errors occurred.";
                 }
@@ -99,8 +97,72 @@ public class OCR {
                 tempTessOutputFile.delete();
                 throw new RuntimeException(msg);
             }
-            
+
         }
+        tempTessOutputFile.delete();
+        return result.toString();
+    }
+
+    /**
+     * @param tempImageFiles
+     * @param lang
+     * @return
+     * @throws java.lang.Exception
+     */
+    String recognizeText(final File tempImageFile, final String lang) throws Exception {
+        File tempTessOutputFile = File.createTempFile(OUTPUT_FILE_NAME, FILE_EXTENSION);
+        String outputFileName = tempTessOutputFile.getPath().substring(0, tempTessOutputFile.getPath().length() - FILE_EXTENSION.length()); // chop the .txt extension
+
+        List<String> cmd = new ArrayList<String>();
+        cmd.add(tessPath + "/tesseract");
+        cmd.add(tempImageFile.getPath());
+        cmd.add(outputFileName);
+        cmd.add(LANG_OPTION);
+        cmd.add(lang);
+
+        ProcessBuilder pb = new ProcessBuilder();
+        pb.directory(new File(System.getProperty("user.home")));
+        pb.redirectErrorStream(true);
+
+        StringBuffer result = new StringBuffer();
+
+//        ProcessBuilder pb = new ProcessBuilder(tessPath + "/tesseract", tempImageFile.getPath(), outputFileName, LANG_OPTION, lang);
+
+        pb.command(cmd);
+        Process process = pb.start();
+
+        int w = process.waitFor();
+        System.out.println("Exit value = " + w);
+
+        if (w == 0) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(tempTessOutputFile), "UTF-8"));
+
+            String str;
+
+            while ((str = in.readLine()) != null) {
+                result.append(str).append(EOL);
+            }
+            in.close();
+        } else {
+            String msg;
+            switch (w) {
+                case 1:
+                    msg = "Errors accessing files.";
+                    break;
+                case 29:
+                    msg = "Cannot recognize the image or its selected region.";
+                    break;
+                case 31:
+                    msg = "Unsupported image format.";
+                    break;
+                default:
+                    msg = "Errors occurred.";
+            }
+
+            tempTessOutputFile.delete();
+            throw new RuntimeException(msg);
+        }
+
         tempTessOutputFile.delete();
         return result.toString();
     }
