e6fb86a0c7501d70ff29ed6cfdd7a1dc2e4df73a
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2009-10-05 01:49:20 +0000

    Backed out the previously added method that takes individual files

--- VietOCR/trunk/src/net/sourceforge/vietocr/OCR.java
+++ VietOCR/trunk/src/net/sourceforge/vietocr/OCR.java
@@ -17,7 +17,6 @@ package net.sourceforge.vietocr;
 
 import java.io.*;
 import java.util.*;
-//import javax.imageio.IIOImage;
 
 /**
  *
@@ -97,70 +96,6 @@ public class OCR {
                 tempTessOutputFile.delete();
                 throw new RuntimeException(msg);
             }
-
-        }
-        tempTessOutputFile.delete();
-        return result.toString();
-    }
-
-    /**
-     * @param tempImageFiles
-     * @param lang
-     * @return
-     * @throws java.lang.Exception
-     */
-    String recognizeText(final File tempImageFile, final String lang) throws Exception {
-        File tempTessOutputFile = File.createTempFile(OUTPUT_FILE_NAME, FILE_EXTENSION);
-        String outputFileName = tempTessOutputFile.getPath().substring(0, tempTessOutputFile.getPath().length() - FILE_EXTENSION.length()); // chop the .txt extension
-
-        List<String> cmd = new ArrayList<String>();
-        cmd.add(tessPath + "/tesseract");
-        cmd.add(tempImageFile.getPath());
-        cmd.add(outputFileName);
-        cmd.add(LANG_OPTION);
-        cmd.add(lang);
-
-        ProcessBuilder pb = new ProcessBuilder();
-        pb.directory(new File(System.getProperty("user.home")));
-        pb.redirectErrorStream(true);
-
-        StringBuffer result = new StringBuffer();
-
-//        ProcessBuilder pb = new ProcessBuilder(tessPath + "/tesseract", tempImageFile.getPath(), outputFileName, LANG_OPTION, lang);
-
-        pb.command(cmd);
-        Process process = pb.start();
-
-        int w = process.waitFor();
-        System.out.println("Exit value = " + w);
-
-        if (w == 0) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(tempTessOutputFile), "UTF-8"));
-
-            String str;
-
-            while ((str = in.readLine()) != null) {
-                result.append(str).append(EOL);
-            }
-            in.close();
-        } else {
-            String msg;
-            switch (w) {
-                case 1:
-                    msg = "Errors accessing files.";
-                    break;
-                case 29:
-                    msg = "Cannot recognize the image or its selected region.";
-                    break;
-                case 31:
-                    msg = "Unsupported image format.";
-                    break;
-                default:
-                    msg = "Errors occurred.";
-            }
-
-            tempTessOutputFile.delete();
-            throw new RuntimeException(msg);
         }
 
         tempTessOutputFile.delete();
