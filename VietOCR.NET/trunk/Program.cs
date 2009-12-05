3c1d1e15f99f2c076a89ce0362d75b53f1a83e2a
Quan Nguyen <nguyenq@users.noreply.github.com>
2009-12-05 17:43:28 +0000

    suppress the black console window after it appears for a split nanosecond

--- VietOCR.NET/trunk/Program.cs
+++ VietOCR.NET/trunk/Program.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace VietOCR.NET
 {
     static class Program
     {
+        [DllImport("kernel32.dll")]
+        public static extern Boolean FreeConsole();
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -18,6 +22,7 @@ namespace VietOCR.NET
             }
             else
             {
+                FreeConsole(); // suppress the black console window after it appears for a split nanosecond
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
                 Application.Run(new GUIWithTools());
