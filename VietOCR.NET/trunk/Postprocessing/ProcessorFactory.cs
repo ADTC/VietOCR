919f9db4f5dc18353375bb833f845b580ed275ce
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2009-12-12 16:35:52 +0000

    More ISO639 codes

--- VietOCR.NET/trunk/Postprocessing/ProcessorFactory.cs
+++ VietOCR.NET/trunk/Postprocessing/ProcessorFactory.cs
@@ -1,14 +1,14 @@
 /**
  * Copyright @ 2008 Quan Nguyen
  * 
- * Licensed under the Apache License, Version 2.0 (the &quot;License&quot;);
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an &quot;AS IS&quot; BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -19,18 +19,12 @@ namespace VietOCR.NET.Postprocessing
 
     public enum ISO639
     {
-        eng, deu, fra, ita, nld, por, spa, vie
+        ben, bul, cat, ces, cop, dan, deu, ell, eng, eus, fin, fra, hun, ind, ita, kan, khm, lav, lit, mal, nld, nor, pol, por, ron, rus, slk, slv, spa, srp, swe, tam, tel, tgl, tur, ukr, vie
     }    
     
-    /**
-     *
-     * @author Quan Nguyen (nguyenq@users.sf.net)
-     */
-
     public class ProcessorFactory
     {
         // ISO369-3 codes: http://www.sil.org/iso639-3/codes.asp
-
 
         public static IPostProcessor createProcessor(ISO639 code)
         {
