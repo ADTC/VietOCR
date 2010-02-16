2cde3b7cef5251553634f624cb08d20ae445d76d
Quan Nguyen <nguyenq@users.noreply.github.com>
2010-02-16 19:37:28 +0000

    Improve post-OCR correction for Viet

--- VietOCR/trunk/src/net/sourceforge/vietocr/postprocessing/ViePP.java
+++ VietOCR/trunk/src/net/sourceforge/vietocr/postprocessing/ViePP.java
@@ -38,8 +38,18 @@ public class ViePP implements IPostProcessor {
 //                .replace("tmg", "úng")
 //                ;
         
-        text = text.replaceAll("(?i)ă(?![cmnpt])", "à")
+        text = text
+                .replaceAll("(?i)(?<=đ)ă\\b", "ã")
+                .replaceAll("(?i)(?<=h)ă\\b", "ả")
+                .replaceAll("(?i)ă(?![cmnpt])", "à")
+                .replaceAll("(?i)ẵ(?=[cpt])", "ắ")
                 .replaceAll("(?<=\\b[Tt])m", "rư")
+                .replaceAll("(?i)\\bl(?=[rh])", "t")
+                .replaceAll("(u|ll|r)(?=[gh])", "n")
+                .replaceAll("(iii|ln|rn)", "m")
+                .replaceAll("(?i)(?<=[qrgsv])ll", "u")
+                .replaceAll("(?i)(?<=[np])ll", "h")
+                .replaceAll("(?i)[oe](?=h)", "c")
                 ;
 
         String nfdText = Normalizer.normalize(text, Normalizer.Form.NFD)
@@ -47,8 +57,12 @@ public class ViePP implements IPostProcessor {
                 .replaceAll("(?i)(?<=u\u031B)(o)(?=" + TONE + "\\p{L})", "$1\u031B") // u+on to u+o+n
                 .replaceAll("(?i)(i)" + TONE + "(?=[eioy])", "$1") // remove mark on i followed by certain vowels
                 .replaceAll("(?i)(?<=[^q]" + VOWEL + "\\p{InCombiningDiacriticalMarks}{0,2})(i)" + TONE + "\\b", "$1") // remove mark on i preceeded by vowels w/ or w/o diacritics
-                .replaceAll("(?i)(?<=[aeo]\u0302)\u2019", "\u0301") // ^right-single-quote to ^acute
+                .replaceAll("(?i)(?<=[aeo]\u0302)['\u2018\u2019]", "\u0301") // ^right-single-quote to ^acute
                 .replaceAll("(?i)\u2018([aeo]\u0302)(?!\\p{InCombiningDiacriticalMarks})", "$1\u0300") // left-single-quote+a^ to a^grave
+                .replaceAll("(?i)(?<=[aeo]\u0302)h", "\u0301n") // a^+h to a^acute+n
+                .replaceAll("(?i)(?<=[uo]" + TONE + ")['\u2018]", "\u031B") // u'+left-single-quote) to u+'
+                .replaceAll("(?i)(?<=" + VOWEL + "\\p{InCombiningDiacriticalMarks}{0,2})l\\b", "t") // vowel+diacritics+l to vowel+diacritics+t
+                .replaceAll("(?i)(?<=" + VOWEL + "\\p{InCombiningDiacriticalMarks}{0,2})ll\\b", "u") // vowel+diacritics+ll to vowel+diacritics+u
                 ;
 
         return Normalizer.normalize(nfdText, Normalizer.Form.NFC);
