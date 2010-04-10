2ae3fcbbb9b434b3f9b3ea2f2808717aaf6eb12d
Quan Nguyen <nguyenq@users.noreply.github.com>
2010-04-10 04:31:24 +0000

    Rearrange All Image Files filter

--- VietOCR/trunk/src/net/sourceforge/vietocr/Gui.java
+++ VietOCR/trunk/src/net/sourceforge/vietocr/Gui.java
@@ -177,7 +177,7 @@ public class Gui extends javax.swing.JFrame {
         currentDirectory = prefs.get("currentDirectory", null);
         filechooser = new JFileChooser(currentDirectory);
         filechooser.setDialogTitle(bundle.getString("jButtonOpen.ToolTipText"));
-        FileFilter allImageFilter = new SimpleFilter("tif;tiff;jpg;jpeg;gif;png;bmp", "All Image Files");
+        FileFilter allImageFilter = new SimpleFilter("bmp;gif;jpg;jpeg;jp2;png;pnm;pbm;pgm;ppm;tif;tiff", "All Image Files");
         FileFilter bmpFilter = new SimpleFilter("bmp", "Bitmap");
         FileFilter gifFilter = new SimpleFilter("gif", "GIF");
         FileFilter jpegFilter = new SimpleFilter("jpg;jpeg", "JPEG");
