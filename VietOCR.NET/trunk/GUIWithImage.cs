fb3a5fc46a9c213557e65769a8208b32c67d14ff
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2010-08-29 12:31:04 +0000

    Save Screenshot mode in registry

--- VietOCR.NET/trunk/GUIWithImage.cs
+++ VietOCR.NET/trunk/GUIWithImage.cs
@@ -5,11 +5,14 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace VietOCR.NET
 {
     public partial class GUIWithImage : VietOCR.NET.GUIWithCommand
     {
+        const string strScreenshot = "Screenshot";
+
         public GUIWithImage()
         {
             InitializeComponent();
@@ -36,6 +39,21 @@ namespace VietOCR.NET
         {
             ToolStripMenuItem mi = (ToolStripMenuItem)sender;
             mi.Checked ^= true;
+        }
+
+        protected override void LoadRegistryInfo(RegistryKey regkey)
+        {
+            base.LoadRegistryInfo(regkey);
+
+            this.screenshotModeToolStripMenuItem.Checked = Convert.ToBoolean(
+                (int)regkey.GetValue(strScreenshot, Convert.ToInt32(false)));
+        }
+
+        protected override void SaveRegistryInfo(RegistryKey regkey)
+        {
+            base.SaveRegistryInfo(regkey);
+
+            regkey.SetValue(strScreenshot, Convert.ToInt32(this.screenshotModeToolStripMenuItem.Checked));
         }
     }
 }
