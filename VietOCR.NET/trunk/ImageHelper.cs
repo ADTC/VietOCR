45db123b16d063938f54f7bcfbf201a580d631dc
Quan Nguyen <nguyenq@users.noreply.github.com>
2010-09-04 01:11:50 +0000

    Changing InterpolationMode from HighCubic to Cubic seems to achieve better results

--- VietOCR.NET/trunk/ImageHelper.cs
+++ VietOCR.NET/trunk/ImageHelper.cs
@@ -14,7 +14,7 @@ namespace VietOCR.NET
             Bitmap bm = new Bitmap((int)(image.Width * dpiX / image.HorizontalResolution), (int)(image.Height * dpiY / image.VerticalResolution));
             bm.SetResolution(dpiX, dpiY);
             Graphics g = Graphics.FromImage(bm);
-            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.InterpolationMode = InterpolationMode.Bicubic;
             g.PixelOffsetMode = PixelOffsetMode.HighQuality;
             g.DrawImage(image, 0, 0);
             g.Dispose();
