50de47b923d7a20ed8ef6fd24a02f786853d6846
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2010-09-27 23:55:21 +0000

    Sort ouput files

--- VietOCR/trunk/src/net/sourceforge/vietocr/PdfUtilities.java
+++ VietOCR/trunk/src/net/sourceforge/vietocr/PdfUtilities.java
@@ -101,6 +101,13 @@ public class PdfUtilities {
             }
         });
 
+        Arrays.sort(workingFiles, new Comparator<File>() {
+            @Override
+            public int compare(File f1, File f2) {
+                return f1.getName().compareTo(f2.getName());
+            }
+        });
+        
         return workingFiles;
     }
 
