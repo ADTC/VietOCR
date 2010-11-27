52d9bc5278c04c7aec9db9231d8be5c03be4d7b1
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2010-11-27 22:48:58 +0000

    Port changes from v2.0

--- VietOCR/branches/VietOCR3/src/net/sourceforge/vietocr/SpellChecker.java
+++ VietOCR/branches/VietOCR3/src/net/sourceforge/vietocr/SpellChecker.java
@@ -83,6 +83,9 @@ public class SpellChecker {
     }
 
     void spellCheck() {
+        Highlighter hi = textComp.getHighlighter();
+        hi.removeAllHighlights();
+
         List<String> words = parseText(textComp.getText());
         List<String> misspelledWords = spellCheck(words);
         if (misspelledWords.isEmpty()) {
@@ -101,9 +104,6 @@ public class SpellChecker {
         Pattern pattern = Pattern.compile(patternStr, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(textComp.getText());
 
-        Highlighter hi = textComp.getHighlighter();
-        hi.removeAllHighlights();
-
         while (matcher.find()) {
             try {
                 hi.addHighlight(matcher.start(), matcher.end(), myPainter);
@@ -115,9 +115,10 @@ public class SpellChecker {
 
     List<String> spellCheck(List<String> words) {
         List<String> misspelled = new ArrayList<String>();
+        
         for (String word : words) {
             if (spellDict.misspelled(word)) {
-                // is mispelled word in user.dic?
+                // is misspelled word in user.dic?
                 if (!userWordList.contains(word.toLowerCase())) {
                     misspelled.add(word);
                 }
@@ -150,6 +151,41 @@ public class SpellChecker {
         if (lstList.size() > 0) {
             this.textComp.getDocument().removeDocumentListener(lstList.remove(0));
             this.textComp.getHighlighter().removeAllHighlights();
+        }
+    }
+
+    public List<String> suggest(String misspelled) {
+        List<String> list = new ArrayList<String>();
+        list.add(misspelled);
+        if (spellCheck(list).isEmpty()) {
+            return null;
+        } else {
+            try {
+                return spellDict.suggest(misspelled);
+            } catch (Exception e) {
+                return null;
+            }
+        }
+    }
+
+    public void ignoreWord(String word) {
+        if (!userWordList.contains(word.toLowerCase())) {
+            userWordList.add(word.toLowerCase());
+        }
+    }
+
+    public void addWord(String word) {
+        if (!userWordList.contains(word.toLowerCase())) {
+            userWordList.add(word.toLowerCase());
+            try {
+                File userDict = new File(baseDir, "dict/user.dic");
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(userDict, true), "UTF8"));
+                out.write(word);
+                out.newLine();
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
