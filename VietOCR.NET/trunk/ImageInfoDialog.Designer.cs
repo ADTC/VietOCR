4739850a2c873c8c1acf16ad6707bdabbe9c3761
Quan Nguyen <nguyenq@users.noreply.github.com>
2010-12-02 03:33:11 +0000

    Adjust location and size of UI controls for various CultureInfo

--- VietOCR.NET/trunk/ImageInfoDialog.Designer.cs
+++ VietOCR.NET/trunk/ImageInfoDialog.Designer.cs
@@ -47,100 +47,153 @@
             // 
             // buttonOK
             // 
-            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOK.AccessibleDescription = null;
+            this.buttonOK.AccessibleName = null;
             resources.ApplyResources(this.buttonOK, "buttonOK");
+            this.buttonOK.BackgroundImage = null;
+            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOK.Font = null;
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.UseVisualStyleBackColor = true;
             // 
             // labelXRes
             // 
+            this.labelXRes.AccessibleDescription = null;
+            this.labelXRes.AccessibleName = null;
             resources.ApplyResources(this.labelXRes, "labelXRes");
+            this.labelXRes.Font = null;
             this.labelXRes.Name = "labelXRes";
             // 
             // textBoxXRes
             // 
+            this.textBoxXRes.AccessibleDescription = null;
+            this.textBoxXRes.AccessibleName = null;
             resources.ApplyResources(this.textBoxXRes, "textBoxXRes");
+            this.textBoxXRes.BackgroundImage = null;
+            this.textBoxXRes.Font = null;
             this.textBoxXRes.Name = "textBoxXRes";
             this.textBoxXRes.ReadOnly = true;
             // 
             // textBoxYRes
             // 
+            this.textBoxYRes.AccessibleDescription = null;
+            this.textBoxYRes.AccessibleName = null;
             resources.ApplyResources(this.textBoxYRes, "textBoxYRes");
+            this.textBoxYRes.BackgroundImage = null;
+            this.textBoxYRes.Font = null;
             this.textBoxYRes.Name = "textBoxYRes";
             this.textBoxYRes.ReadOnly = true;
             // 
             // labelYRes
             // 
+            this.labelYRes.AccessibleDescription = null;
+            this.labelYRes.AccessibleName = null;
             resources.ApplyResources(this.labelYRes, "labelYRes");
+            this.labelYRes.Font = null;
             this.labelYRes.Name = "labelYRes";
             // 
             // textBoxWidth
             // 
+            this.textBoxWidth.AccessibleDescription = null;
+            this.textBoxWidth.AccessibleName = null;
             resources.ApplyResources(this.textBoxWidth, "textBoxWidth");
+            this.textBoxWidth.BackgroundImage = null;
+            this.textBoxWidth.Font = null;
             this.textBoxWidth.Name = "textBoxWidth";
             this.textBoxWidth.ReadOnly = true;
             // 
             // labelWidth
             // 
+            this.labelWidth.AccessibleDescription = null;
+            this.labelWidth.AccessibleName = null;
             resources.ApplyResources(this.labelWidth, "labelWidth");
+            this.labelWidth.Font = null;
             this.labelWidth.Name = "labelWidth";
             // 
             // textBoxHeight
             // 
+            this.textBoxHeight.AccessibleDescription = null;
+            this.textBoxHeight.AccessibleName = null;
             resources.ApplyResources(this.textBoxHeight, "textBoxHeight");
+            this.textBoxHeight.BackgroundImage = null;
+            this.textBoxHeight.Font = null;
             this.textBoxHeight.Name = "textBoxHeight";
             this.textBoxHeight.ReadOnly = true;
             // 
             // labelHeight
             // 
+            this.labelHeight.AccessibleDescription = null;
+            this.labelHeight.AccessibleName = null;
             resources.ApplyResources(this.labelHeight, "labelHeight");
+            this.labelHeight.Font = null;
             this.labelHeight.Name = "labelHeight";
             // 
             // buttonCancel
             // 
-            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.AccessibleDescription = null;
+            this.buttonCancel.AccessibleName = null;
             resources.ApplyResources(this.buttonCancel, "buttonCancel");
+            this.buttonCancel.BackgroundImage = null;
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Font = null;
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
             // 
             // label5
             // 
+            this.label5.AccessibleDescription = null;
+            this.label5.AccessibleName = null;
             resources.ApplyResources(this.label5, "label5");
+            this.label5.Font = null;
             this.label5.Name = "label5";
             // 
             // label6
             // 
+            this.label6.AccessibleDescription = null;
+            this.label6.AccessibleName = null;
             resources.ApplyResources(this.label6, "label6");
+            this.label6.Font = null;
             this.label6.Name = "label6";
             // 
             // comboBox4
             // 
+            this.comboBox4.AccessibleDescription = null;
+            this.comboBox4.AccessibleName = null;
+            resources.ApplyResources(this.comboBox4, "comboBox4");
+            this.comboBox4.BackgroundImage = null;
             this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.Font = null;
             this.comboBox4.FormattingEnabled = true;
             this.comboBox4.Items.AddRange(new object[] {
             resources.GetString("comboBox4.Items"),
             resources.GetString("comboBox4.Items1"),
             resources.GetString("comboBox4.Items2")});
-            resources.ApplyResources(this.comboBox4, "comboBox4");
             this.comboBox4.Name = "comboBox4";
             this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
             // 
             // comboBox3
             // 
+            this.comboBox3.AccessibleDescription = null;
+            this.comboBox3.AccessibleName = null;
+            resources.ApplyResources(this.comboBox3, "comboBox3");
+            this.comboBox3.BackgroundImage = null;
             this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.Font = null;
             this.comboBox3.FormattingEnabled = true;
             this.comboBox3.Items.AddRange(new object[] {
             resources.GetString("comboBox3.Items"),
             resources.GetString("comboBox3.Items1"),
             resources.GetString("comboBox3.Items2")});
-            resources.ApplyResources(this.comboBox3, "comboBox3");
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
             // 
             // ImageInfoDialog
             // 
+            this.AccessibleDescription = null;
+            this.AccessibleName = null;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = null;
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.comboBox4);
@@ -155,7 +208,9 @@
             this.Controls.Add(this.textBoxXRes);
             this.Controls.Add(this.labelXRes);
             this.Controls.Add(this.buttonOK);
+            this.Font = null;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = null;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ImageInfoDialog";
