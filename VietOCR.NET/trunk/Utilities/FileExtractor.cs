68622e2396c7e46f8abe355bc5547dff648aff34
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2010-12-04 14:47:11 +0000

    Add a FileExtractor wrapper utility

---
+++ VietOCR.NET/trunk/Utilities/FileExtractor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ICSharpCode.SharpZipLib.Tar;
+using ICSharpCode.SharpZipLib.GZip;
+
+namespace VietOCR.NET.Utilities
+{
+    class FileExtractor
+    {
+        public static void ExtractTGZ(String gzArchiveName, String destFolder)
+        {
+            Stream inStream = File.OpenRead(gzArchiveName);
+            Stream gzipStream = new GZipInputStream(inStream);
+
+            TarArchive tarArchive = TarArchive.CreateInputTarArchive(gzipStream);
+            tarArchive.ExtractContents(destFolder);
+            tarArchive.Close();
+
+            gzipStream.Close();
+            inStream.Close();
+        }
+    }
+}
