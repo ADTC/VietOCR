2456a0e2c0dd5c305416f103a1df15084c6792f7
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2010-12-09 05:22:10 +0000

    Merge from v2.0

--- VietOCR/branches/VietOCR3/src/net/sourceforge/vietocr/GuiWithFormat.java
+++ VietOCR/branches/VietOCR3/src/net/sourceforge/vietocr/GuiWithFormat.java
@@ -51,7 +51,6 @@ public class GuiWithFormat extends GuiWithImage {
         FontDialog dlg = new FontDialog(this);
         dlg.setAttributes(font);
 
-        File baseDir = Utilities.getBaseDir(this);
         Properties prop = new Properties();
 
         try {
@@ -59,7 +58,7 @@ public class GuiWithFormat extends GuiWithImage {
             prop.loadFromXML(new FileInputStream(xmlFile));
             dlg.setPreviewText(prop.getProperty(langCode));
         } catch (IOException ioe) {
-            JOptionPane.showMessageDialog(null, "Missing pangram.xml file. Cannot find it in " + new File(baseDir, "data").getPath() + " directory.", APP_NAME, JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, ioe.getMessage(), APP_NAME, JOptionPane.ERROR_MESSAGE);
             ioe.printStackTrace();
         } catch (Exception exc) {
             exc.printStackTrace();
