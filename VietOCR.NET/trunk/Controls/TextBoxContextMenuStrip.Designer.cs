bfd9dd0f0d66331db60c96c2c286d7c0168c3176
Quan Nguyen <nguyenq@users.noreply.github.com>
2011-01-03 01:48:59 +0000

    Localize textbox's context menu

--- VietOCR.NET/trunk/Controls/TextBoxContextMenuStrip.Designer.cs
+++ VietOCR.NET/trunk/Controls/TextBoxContextMenuStrip.Designer.cs
@@ -37,6 +37,7 @@ namespace VietOCR.NET.Controls
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TextBoxContextMenuStrip));
             this.miUndo = new System.Windows.Forms.ToolStripMenuItem();
             this.miSeparator = new System.Windows.Forms.ToolStripSeparator();
             this.miCut = new System.Windows.Forms.ToolStripMenuItem();
@@ -45,49 +46,101 @@ namespace VietOCR.NET.Controls
             this.miDelete = new System.Windows.Forms.ToolStripMenuItem();
             this.miSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.miSelectAll = new System.Windows.Forms.ToolStripMenuItem();
-            //
+            this.SuspendLayout();
+            // 
             // miUndo
-            //
-            this.miUndo.Text = "&Undo";
+            // 
+            this.miUndo.AccessibleDescription = null;
+            this.miUndo.AccessibleName = null;
+            resources.ApplyResources(this.miUndo, "miUndo");
+            this.miUndo.BackgroundImage = null;
+            this.miUndo.Name = "miUndo";
+            this.miUndo.ShortcutKeyDisplayString = null;
             this.miUndo.Click += new System.EventHandler(this.miUndo_Click);
-            //
+            // 
+            // miSeparator
+            // 
+            this.miSeparator.AccessibleDescription = null;
+            this.miSeparator.AccessibleName = null;
+            resources.ApplyResources(this.miSeparator, "miSeparator");
+            this.miSeparator.Name = "miSeparator";
+            // 
             // miCut
-            //
-            this.miCut.Text = "Cu&t";
+            // 
+            this.miCut.AccessibleDescription = null;
+            this.miCut.AccessibleName = null;
+            resources.ApplyResources(this.miCut, "miCut");
+            this.miCut.BackgroundImage = null;
+            this.miCut.Name = "miCut";
+            this.miCut.ShortcutKeyDisplayString = null;
             this.miCut.Click += new System.EventHandler(this.miCut_Click);
-            //
+            // 
             // miCopy
-            //
-            this.miCopy.Text = "&Copy";
+            // 
+            this.miCopy.AccessibleDescription = null;
+            this.miCopy.AccessibleName = null;
+            resources.ApplyResources(this.miCopy, "miCopy");
+            this.miCopy.BackgroundImage = null;
+            this.miCopy.Name = "miCopy";
+            this.miCopy.ShortcutKeyDisplayString = null;
             this.miCopy.Click += new System.EventHandler(this.miCopy_Click);
-            //
+            // 
             // miPaste
-            //
-            this.miPaste.Text = "&Paste";
+            // 
+            this.miPaste.AccessibleDescription = null;
+            this.miPaste.AccessibleName = null;
+            resources.ApplyResources(this.miPaste, "miPaste");
+            this.miPaste.BackgroundImage = null;
+            this.miPaste.Name = "miPaste";
+            this.miPaste.ShortcutKeyDisplayString = null;
             this.miPaste.Click += new System.EventHandler(this.miPaste_Click);
-            //
+            // 
             // miDelete
-            //
-            this.miDelete.Text = "&Delete";
+            // 
+            this.miDelete.AccessibleDescription = null;
+            this.miDelete.AccessibleName = null;
+            resources.ApplyResources(this.miDelete, "miDelete");
+            this.miDelete.BackgroundImage = null;
+            this.miDelete.Name = "miDelete";
+            this.miDelete.ShortcutKeyDisplayString = null;
             this.miDelete.Click += new System.EventHandler(this.miDelete_Click);
-            //
+            // 
+            // miSeparator2
+            // 
+            this.miSeparator2.AccessibleDescription = null;
+            this.miSeparator2.AccessibleName = null;
+            resources.ApplyResources(this.miSeparator2, "miSeparator2");
+            this.miSeparator2.Name = "miSeparator2";
+            // 
             // miSelectAll
-            //
-            this.miSelectAll.Text = "Select &All";
+            // 
+            this.miSelectAll.AccessibleDescription = null;
+            this.miSelectAll.AccessibleName = null;
+            resources.ApplyResources(this.miSelectAll, "miSelectAll");
+            this.miSelectAll.BackgroundImage = null;
+            this.miSelectAll.Name = "miSelectAll";
+            this.miSelectAll.ShortcutKeyDisplayString = null;
             this.miSelectAll.Click += new System.EventHandler(this.miSelectAll_Click);
-
+            // 
+            // TextBoxContextMenuStrip
+            // 
+            this.AccessibleDescription = null;
+            this.AccessibleName = null;
+            resources.ApplyResources(this, "$this");
+            this.BackgroundImage = null;
+            this.Font = null;
             this.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-                this.miUndo,
-                this.miSeparator,
-                this.miCut,
-                this.miCopy,
-                this.miPaste,
-                this.miDelete,
-                this.miSeparator2,
-                this.miSelectAll
-            });
+            this.miUndo,
+            this.miSeparator,
+            this.miCut,
+            this.miCopy,
+            this.miPaste,
+            this.miDelete,
+            this.miSeparator2,
+            this.miSelectAll});
+            this.Opening += new System.ComponentModel.CancelEventHandler(this.StandardTextBoxContextMenuStrip_Opening);
+            this.ResumeLayout(false);
 
-            this.Opening += new System.ComponentModel.CancelEventHandler(StandardTextBoxContextMenuStrip_Opening);
         }
 
         public void RepopulateContextMenu()
