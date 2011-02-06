ce2805c95c437abdcacd0f6306a032396e90edaf
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2011-02-06 04:51:34 +0000

    Localize "Browse" tooltiptext

--- VietOCR.NET/trunk/SplitPdfDialog.cs
+++ VietOCR.NET/trunk/SplitPdfDialog.cs
@@ -26,8 +26,8 @@ namespace VietOCR.NET
         {
             InitializeComponent();
             disableBoxes(!this.radioButtonPages.Checked);
-            this.toolTip1.SetToolTip(this.buttonBrowseInput, "Browse");
-            this.toolTip1.SetToolTip(this.buttonBrowseOutput, "Browse");
+            this.toolTip1.SetToolTip(this.buttonBrowseInput, Properties.Resources.Browse);
+            this.toolTip1.SetToolTip(this.buttonBrowseOutput, Properties.Resources.Browse);
         }
 
         private void radioButtonPages_CheckedChanged(object sender, EventArgs e)
