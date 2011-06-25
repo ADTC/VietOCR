3c29c6977d142e1d6981f1b072edfdba56d2e4b2
nguyenq <nguyenq@5968eacb-d6fe-4cef-bb96-d5b655664892>
2011-06-25 13:13:22 +0000

    Reserve for future implementation using DLL

---
+++ VietOCR.NET/branches/VietOCR3.NET/OCRImages.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace VietOCR.NET
+{
+    class OCRImages : OCR<Image>
+    {
+        /// <summary>
+        /// Recognize text
+        /// </summary>
+        /// <param name="imageEntities"></param>
+        /// <param name="index"></param>
+        /// <param name="lang"></param>
+        /// <returns></returns>
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        public override string RecognizeText(IList<Image> imageEntities, string lang)
+        {
+            //tessnet3.Tesseract ocr = new tessnet3.Tesseract();
+
+            //ocr.Init(null, lang, 3);
+
+            //StringBuilder strB = new StringBuilder();
+
+            //foreach (Image image in imageEntities)
+            //{
+            //    string result = ocr.DoOCR(image, rect);
+
+            //    if (result == null) return String.Empty;
+            //    strB.Append(result);
+
+            //}
+            //return strB.ToString();
+            return null;
+        }
+    }
+}
